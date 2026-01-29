89b803e758b3d38d5d5e8c880e61938d960f59e3
raed09321@gmail.com <raed09321@gmail.com>
2026-01-29 11:33:39 +0300

    style(paper): enhance Paper component styling with shadow and padding

--- src/components/paper/Paper.tsx
+++ src/components/paper/Paper.tsx
@@ -3,7 +3,12 @@ import { cn } from "src/utils/cn";
 export type PaperProps = React.HTMLAttributes<HTMLDivElement>;
 
 function Paper({ className, ...props }: PaperProps) {
-  return <div className={cn("bg-white rounded-xl ", className)} {...props} />;
+  return (
+    <div
+      className={cn("bg-white rounded-xl shadow-sm p-5", className)}
+      {...props}
+    />
+  );
 }
 
 export default Paper;
