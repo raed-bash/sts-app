5a4f378418afb3a703c534148b4a87fc8cdb977c
raed09321@gmail.com <raed09321@gmail.com>
2026-02-04 09:55:56 +0300

    feat(profile): create ProfileMenu compnent from the Profile compnent and close the menu on focusout

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import IconButton from "./buttons/IconButton";
 import ProfileIcon from "src/assets/icons/profile.svg?react";
 import ProfileFilledIcon from "src/assets/icons/profile-filled.svg?react";
@@ -13,20 +13,21 @@ import useLogout from "src/hooks/useLogout";
 import Animation from "./Animation";
 import { cn } from "src/utils/cn";
 import AppLink from "./AppLink";
+import useFocusout from "src/hooks/useFocusout";
 
 export default function Profile() {
-  const handleLogout = useLogout();
   const [openProfile, setOpenProfile] = useState(false);
 
   const handleToggleProfile = () => {
     setOpenProfile((openProfile) => !openProfile);
   };
 
-  const meQuery = useQuery({ queryKey: ["me"], queryFn: () => usersApi.me() });
+  const profileMenuRef = useRef(null);
 
-  const me = meQuery.data;
+  useFocusout(profileMenuRef, () => setOpenProfile(false));
+
   return (
-    <div className="relative">
+    <div className="relative" ref={profileMenuRef}>
       <IconButton
         onClick={handleToggleProfile}
         className="aria-expanded:bg-[#DFDFDF]/40"
@@ -34,59 +35,71 @@ export default function Profile() {
       >
         <ProfileIcon className="fill-(--text)" />
       </IconButton>
-      <Animation
-        isOpen={openProfile}
-        className="absolute flex gap-8 flex-col justify-center items-center top-[160%] right-0  min-w-[218px] min-h-32 bg-white shadow-base rounded-lg"
-        tabIndex={0}
-      >
-        {meQuery.isLoading ? (
-          <Loading />
-        ) : (
-          <div className="w-full flex flex-col gap-5">
-            <div className="flex items-center gap-2 px-4 py-3 border-b border-gray-300 mb-3">
-              <div className="w-[45px] h-[45px]">
-                <ProfileIcon className="fill-(--text) w-full h-full" />
-              </div>
-              <div className="flex justify-center items-start flex-col">
-                <p className="text-sm">{me?.username}</p>
-                <RoleView role={me?.role} className="text-xs" />
-              </div>
+      <ProfileMenu open={openProfile} />
+    </div>
+  );
+}
+
+function ProfileMenu({ open }: { open: boolean }) {
+  const handleLogout = useLogout();
+
+  const meQuery = useQuery({ queryKey: ["me"], queryFn: () => usersApi.me() });
+
+  const me = meQuery.data;
+
+  return (
+    <Animation
+      isOpen={open}
+      className="absolute flex gap-8 flex-col justify-center items-center top-[160%] right-0  min-w-[218px] min-h-32 bg-white shadow-base rounded-lg"
+      tabIndex={0}
+    >
+      {meQuery.isLoading ? (
+        <Loading />
+      ) : (
+        <div className="w-full flex flex-col gap-5">
+          <div className="flex items-center gap-2 px-4 py-3 border-b border-gray-300 mb-3">
+            <div className="w-[45px] h-[45px]">
+              <ProfileIcon className="fill-(--text) w-full h-full" />
             </div>
-            <div className="px-4 flex flex-col gap-5">
-              {profileOptions.map((opt) => (
-                <AppLink
-                  to={opt.to}
-                  className="flex items-center gap-3 group duration-75 cursor-pointer  no-underline"
-                >
-                  <span
-                    className={cn(
-                      "h-9 w-9 inline-flex items-center justify-center group-hover:scale-110 transition-all duration-200  rounded-full text-2xl  text-white  ",
-                      opt.bgColorClassName,
-                    )}
-                  >
-                    <opt.Icon className="fill-white " />
-                  </span>
-                  <span className="block text-sm group-hover:text-(--primary) capitalize text-gray-700 ">
-                    {opt.label}
-                  </span>
-                </AppLink>
-              ))}
-            </div>
-            <div className="px-4 py-3 border-t border-gray-300">
-              <Button
-                variant="contained"
-                color="primary"
-                className="flex gap-2 items-center justify-center w-full h-10 text-sm"
-                onClick={handleLogout}
-              >
-                <LogoutIcon className="fill-white  w-5 h-5" />
-                Logout
-              </Button>
+            <div className="flex justify-center items-start flex-col">
+              <p className="text-sm">{me?.username}</p>
+              <RoleView role={me?.role} className="text-xs" />
             </div>
           </div>
-        )}
-      </Animation>
-    </div>
+          <div className="px-4 flex flex-col gap-5">
+            {profileOptions.map((opt) => (
+              <AppLink
+                to={opt.to}
+                className="flex items-center gap-3 group duration-75 cursor-pointer  no-underline"
+              >
+                <span
+                  className={cn(
+                    "h-9 w-9 inline-flex items-center justify-center group-hover:scale-110 transition-all duration-200  rounded-full text-2xl  text-white  ",
+                    opt.bgColorClassName,
+                  )}
+                >
+                  <opt.Icon className="fill-white " />
+                </span>
+                <span className="block text-sm group-hover:text-(--primary) capitalize text-gray-700 ">
+                  {opt.label}
+                </span>
+              </AppLink>
+            ))}
+          </div>
+          <div className="px-4 py-3 border-t border-gray-300">
+            <Button
+              variant="contained"
+              color="primary"
+              className="flex gap-2 items-center justify-center w-full h-10 text-sm"
+              onClick={handleLogout}
+            >
+              <LogoutIcon className="fill-white  w-5 h-5" />
+              Logout
+            </Button>
+          </div>
+        </div>
+      )}
+    </Animation>
   );
 }
 
