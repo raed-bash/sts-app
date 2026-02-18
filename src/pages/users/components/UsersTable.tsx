43e766fab077572a12a837ea3b7d7db57c27a4f0
raed09321@gmail.com <raed09321@gmail.com>
2026-02-18 20:14:25 +0300

    refactor(users): extract UsersTable component from UsersList

---
+++ src/pages/users/components/UsersTable.tsx
@@ -0,0 +1,101 @@
+import Table, { type TableSortStatuses } from "src/components/table/Table";
+import type { UserDto } from "../dtos/user.dto";
+import Tooltip from "src/components/tooltip/Tooltip";
+import { dateFormater } from "src/utils/dateFormater";
+import RoleView from "src/components/RoleView";
+import StatusView from "src/components/StatusView";
+import EditIcon from "src/assets/icons/edit.svg?react";
+import RemoveIcon from "src/assets/icons/remove.svg?react";
+import type { UseTableUtilsSortEventHandler } from "src/components/table/hooks/useTableUtils";
+
+export type UsersTableProps = {
+  handleSortChange: UseTableUtilsSortEventHandler;
+  sorts: TableSortStatuses;
+  setSelectedRows: (sortsStatuses: Set<string | number>) => void;
+  selectedRows: Set<string | number>;
+  hiddenColumns: Set<string>;
+  setHiddenColumns: (hiddenColumns: Set<string>) => void;
+  setPage: (page: number) => void;
+  count: number;
+  rows?: UserDto[];
+  page: number;
+  perPage: number;
+};
+
+export default function UsersTable(props: UsersTableProps) {
+  return (
+    <Table<UserDto>
+      sortStatuses={props.sorts}
+      onSortChange={props.handleSortChange}
+      selectable
+      onSelectRows={props.setSelectedRows}
+      selectedRows={props.selectedRows}
+      hideableColumns
+      hiddenColumns={props.hiddenColumns}
+      setHiddenColumns={props.setHiddenColumns}
+      onPageChange={props.setPage}
+      columns={[
+        {
+          name: "id",
+          headerName: "#",
+        },
+        {
+          name: "username",
+          headerName: "Username",
+        },
+        {
+          name: "full_name",
+          headerName: "Full Name",
+          getCell(_, row) {
+            return row.student?.full_name || row.teacher?.full_name;
+          },
+        },
+        {
+          name: "status",
+          headerName: "Status",
+          getCell(status) {
+            return <StatusView status={status} />;
+          },
+        },
+        {
+          name: "role",
+          headerName: "Role",
+          getCell(role) {
+            return <RoleView role={role} />;
+          },
+        },
+        {
+          name: "created_at",
+          headerName: "Created at",
+          getCell(value) {
+            return dateFormater(value);
+          },
+        },
+        {
+          name: "actions",
+          headerName: "Actions",
+          getCell() {
+            return (
+              <div className="flex gap-3 justify-start  ">
+                <Tooltip title="Edit" placement="top">
+                  {/* <IconButton> */}
+                  <EditIcon />
+                  {/* </IconButton> */}
+                </Tooltip>
+                <Tooltip title="Remove" placement="top">
+                  {/* <IconButton> */}
+                  <RemoveIcon />
+                  {/* </IconButton> */}
+                </Tooltip>
+              </div>
+            );
+          },
+        },
+      ]}
+      rows={props.rows || []}
+      count={props.count}
+      currentPage={props.page}
+      perPage={props.perPage}
+    />
+  );
+}
--- src/pages/users/pages/UsersList.tsx
+++ src/pages/users/pages/UsersList.tsx
@@ -1,28 +1,19 @@
 import { useQuery } from "@tanstack/react-query";
 import { usersApi } from "../users.api";
-import Table from "src/components/table/Table";
-import type { UserDto } from "../dtos/user.dto";
-import StatusView from "src/components/StatusView";
-import RoleView from "src/components/RoleView";
-import { dateFormater } from "src/utils/dateFormater";
-import InputPlus from "src/components/inputs/InputPlus";
-import AddIcon from "src/assets/icons/add.svg?react";
-import EditIcon from "src/assets/icons/edit.svg?react";
-import RemoveIcon from "src/assets/icons/remove.svg?react";
-import IconButton from "src/components/buttons/IconButton";
-import Tooltip from "src/components/tooltip/Tooltip";
 import { UserPages } from "../users.pages";
 import Paper from "src/components/paper/Paper";
 import useSelectRows from "src/hooks/useSelectRows";
 import useHiddenColumnsLocalStorage from "src/hooks/useHiddenColumnsLocalStorage";
 import { useState } from "react";
 import useSorts from "src/hooks/useSorts";
+import UsersTable from "../components/UsersTable";
 
 export default function UsersList() {
   const usersQuery = useQuery({
     queryKey: [UserPages.users.key],
     queryFn: () => usersApi.getUsers(),
   });
+
   const { selectedRows, setSelectedRows } = useSelectRows(
     "any",
     new Set<number | string>(),
@@ -32,90 +23,24 @@ export default function UsersList() {
     useHiddenColumnsLocalStorage("nmae");
 
   const { handleSortChange, sorts } = useSorts("users");
+
   return (
     <div>
       <h1 className="text-3xl font-bold mb-4">Users</h1>
-      <div className="flex justify-between py-5">
-        <InputPlus type="text" title="Search:" oneline />
-        <Tooltip title="Create">
-          <IconButton className="w-min ">
-            <AddIcon />
-          </IconButton>
-        </Tooltip>
-      </div>
+
       <Paper className="p-0">
-        <Table<UserDto>
-          sortStatuses={sorts}
-          onSortChange={handleSortChange}
-          selectable
-          onSelectRows={setSelectedRows}
-          selectedRows={selectedRows}
-          hideableColumns
+        <UsersTable
+          handleSortChange={handleSortChange}
           hiddenColumns={hiddenColumns}
+          page={page}
+          selectedRows={selectedRows}
           setHiddenColumns={setHiddenColumns}
-          onPageChange={setPage}
-          columns={[
-            {
-              name: "id",
-              headerName: "#",
-            },
-            {
-              name: "username",
-              headerName: "Username",
-            },
-            {
-              name: "full_name",
-              headerName: "Full Name",
-              getCell(_, row) {
-                return row.student?.full_name || row.teacher?.full_name;
-              },
-            },
-            {
-              name: "status",
-              headerName: "Status",
-              getCell(status) {
-                return <StatusView status={status} />;
-              },
-            },
-            {
-              name: "role",
-              headerName: "Role",
-              getCell(role) {
-                return <RoleView role={role} />;
-              },
-            },
-            {
-              name: "created_at",
-              headerName: "Created at",
-              getCell(value) {
-                return dateFormater(value);
-              },
-            },
-            {
-              name: "actions",
-              headerName: "Actions",
-              getCell() {
-                return (
-                  <div className="flex gap-3 justify-start  ">
-                    <Tooltip title="Edit" placement="top">
-                      {/* <IconButton> */}
-                      <EditIcon />
-                      {/* </IconButton> */}
-                    </Tooltip>
-                    <Tooltip title="Remove" placement="top">
-                      {/* <IconButton> */}
-                      <RemoveIcon />
-                      {/* </IconButton> */}
-                    </Tooltip>
-                  </div>
-                );
-              },
-            },
-          ]}
-          rows={usersQuery.data?.data || []}
-          count={30}
-          currentPage={page}
-          perPage={2}
+          setPage={setPage}
+          setSelectedRows={setSelectedRows}
+          sorts={sorts}
+          count={10}
+          perPage={10}
+          rows={usersQuery.data?.data}
         />
       </Paper>
     </div>
