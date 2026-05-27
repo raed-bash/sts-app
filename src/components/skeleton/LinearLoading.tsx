c6e2a672c6287737137adc1c2ff04015c9eee9b3
raed09321@gmail.com <raed09321@gmail.com>
2026-05-27 22:14:02 +0300

    fix(linear-loading): update background color class for loader line to use CSS variable

--- src/components/skeleton/LinearLoading.tsx
+++ src/components/skeleton/LinearLoading.tsx
@@ -5,7 +5,7 @@ export type LinearLoadingProps = React.HTMLAttributes<HTMLDivElement>;
 function LinearLoading(props: LinearLoadingProps) {
   return (
     <div {...props}>
-      <div className="loader-line before:bg-primary-main"></div>
+      <div className="loader-line before:bg-(--primary)"></div>
     </div>
   );
 }
