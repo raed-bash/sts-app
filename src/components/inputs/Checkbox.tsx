94831fc0356b4bb4889c4046caec2b00e4905715
raed09321@gmail.com <raed09321@gmail.com>
2026-05-27 22:10:22 +0300

    refactor(checkbox): simplify Checkbox component by removing unnecessary elements and props

--- src/components/inputs/Checkbox.tsx
+++ src/components/inputs/Checkbox.tsx
@@ -1,15 +1,10 @@
 import { cn } from "src/utils/cn";
 import Input from "./Input";
-import { useId } from "react";
 
 export type CheckboxProps = Omit<
   React.InputHTMLAttributes<HTMLInputElement>,
   "type"
 > & {
-  disabled?: boolean;
-
-  className?: string;
-
   secondaryStatus?: boolean;
 };
 
@@ -18,31 +13,25 @@ function Checkbox({
   secondaryStatus = false,
   ...props
 }: CheckboxProps) {
-  const randomId = useId();
-  const id = props.id || randomId;
-
   return (
-    <div className="w-4 h-4">
-      <label
-        htmlFor={id}
-        className={cn(
-          "w-full h-full relative duration-150 before:duration-150 after:duration-150 inline-flex justify-center items-center bg-(--secondary)/10 rounded border border-(--secondary)/50 hover:border-(--primary)",
-          "aria-checked:bg-(--primary) data-[secondary=true]:bg-(--primary)",
-          "aria-checked:before:content-[''] before:absolute aria-checked:before:w-2.5 before:h-[1.5px] before:bg-white aria-checked:before:-rotate-52 before:right-px",
-          "aria-checked:after:content-[''] after:absolute aria-checked:after:w-1 after:-translate-x-[3.5px] after:translate-y-[1.6px] after:h-[1.5px] after:bg-white after:rotate-45",
-          "data-[secondary=true]:before:w-2.5 data-[secondary=true]:before:rotate-0 data-[secondary=true]:before:left-[50%] data-[secondary=true]:before:-translate-x-[50%] ",
-        )}
-        aria-checked={!secondaryStatus && props.checked}
-        data-secondary={secondaryStatus}
-      ></label>
-      <Input
-        type="checkbox"
-        className={cn("", className)}
-        hidden
-        id={id}
-        {...props}
-      />
-    </div>
+    <Input
+      type="checkbox"
+      className={cn(
+        "appearance-none w-4 h-4 relative duration-150 before:duration-150 after:duration-150 inline-flex justify-center items-center bg-(--secondary)/10 rounded border border-(--secondary)/50 hover:border-(--primary)",
+
+        "checked:bg-(--primary) data-[secondary=true]:bg-(--primary)",
+
+        "checked:before:content-[''] before:absolute checked:before:w-2.5 before:h-[1.5px] before:bg-white checked:before:-rotate-52 before:right-px",
+
+        "checked:after:content-[''] after:absolute checked:after:w-1 after:-translate-x-[3.5px] after:translate-y-[1.6px] after:h-[1.5px] after:bg-white after:rotate-45",
+
+        "data-[secondary=true]:before:w-2.5 data-[secondary=true]:before:rotate-0 data-[secondary=true]:before:left-[50%] data-[secondary=true]:before:-translate-x-[50%] ",
+        className
+      )}
+      aria-checked={!secondaryStatus && props.checked}
+      data-secondary={secondaryStatus}
+      {...props}
+    />
   );
 }
 
