e497dad3d5b8caeec49221eaf0a3422a358d1720
raed-bash <raed09321@gmail.com>
2026-09-01 11:53:27 +0300

    chore(eslint): add eslint-plugin-react-compiler and update package-lock.json
    - Added `eslint-plugin-react-compiler` version `19.1.0-rc.2` to dependencies in `package.json`.
    - Updated `package-lock.json` to include new dependencies and removed `dev` flags from several packages.
    - Noted deprecation of `@babel/plugin-proposal-private-methods` and suggested using `@babel/plugin-transform-private-methods` instead.

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import js from "@eslint/js";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
+import eslintPluginReactCompiler from "eslint-plugin-react-compiler";
 import tseslint from "typescript-eslint";
 import { defineConfig, globalIgnores } from "eslint/config";
 
@@ -12,15 +13,17 @@ export default defineConfig([
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
+      reactHooks.configs.flat["recommended-latest"],
       reactRefresh.configs.vite,
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    plugins: { "react-compiler": eslintPluginReactCompiler },
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
+      "react-compiler/react-compiler": "error",
     },
   },
 ]);
