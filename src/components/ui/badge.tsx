50c640dd29eb8fbc8a087d9bb8ab3d6da8447cf8
raed-bash <raed09321@gmail.com>
2026-09-01 12:14:53 +0300

    refactor(lib): move utils file from src/lib to src/components/lib and update imports in UI components

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -2,7 +2,7 @@ import { mergeProps } from "@base-ui/react/merge-props";
 import { useRender } from "@base-ui/react/use-render";
 import { type VariantProps } from "class-variance-authority";
 
-import { cn } from "@/lib/utils";
+import { cn } from "../lib";
 import { badgeVariants } from "./badge-variants";
 
 function Badge({
