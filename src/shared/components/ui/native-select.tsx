9b2885b050fa1576e76b310866e8634775b8d3da
raed-bash <raed09321@gmail.com>
2026-09-10 15:58:11 +0300

    refactor(NativeSelect): remove unused aria-placeholder and clean up exports

--- src/shared/components/ui/native-select.tsx
+++ src/shared/components/ui/native-select.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-
 import { cn } from "cn";
 import { ChevronDownIcon } from "lucide-react";
 
@@ -25,7 +24,6 @@ function NativeSelect({
         data-slot="native-select"
         data-size={size}
         className="h-8 w-full min-w-0 appearance-none rounded-lg border border-input bg-transparent py-1 pr-8 pl-2.5 text-sm transition-colors outline-none select-none selection:bg-primary selection:text-primary-foreground placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-3 focus-visible:ring-ring/50 disabled:pointer-events-none disabled:cursor-not-allowed aria-invalid:border-destructive aria-invalid:ring-3 aria-invalid:ring-destructive/20 data-[size=sm]:h-7 data-[size=sm]:rounded-[min(var(--radius-md),10px)] data-[size=sm]:py-0.5 dark:bg-input/30 dark:hover:bg-input/50 dark:aria-invalid:border-destructive/50 dark:aria-invalid:ring-destructive/40"
-        aria-placeholder="asdasdas"
         {...props}
       />
       <ChevronDownIcon
@@ -62,8 +60,5 @@ function NativeSelectOptGroup({
     />
   );
 }
-
-NativeSelect.NativeSelectOption = NativeSelectOption;
-NativeSelect.NativeSelectOptGroup = NativeSelectOptGroup;
 
 export { NativeSelect, NativeSelectOptGroup, NativeSelectOption };
