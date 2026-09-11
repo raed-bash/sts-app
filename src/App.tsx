ce24660c0ce77c008d41e29a8a41d5addd378b11
raed09321@gmail.com <raed09321@gmail.com>
2026-09-11 17:19:21 +0300

    feat(ReactQueryDevtools): add React Query Devtools for improved debugging and development experience

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,12 @@ import ThemeProvider from "./contexts/ThemeProvider";
 import { TooltipProvider } from "./shared/components/ui/tooltip";
 import AppRouter from "./app/app.router";
 import { queryClient } from "./lib/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
+      {import.meta.env.DEV && <ReactQueryDevtools />}
       <ThemeProvider>
         <AuthProvider>
           <TooltipProvider>
