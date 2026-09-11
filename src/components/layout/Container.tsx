6d3a51f5a0b828f381d20b5a2eb4e2a53e11016e
raed09321@gmail.com <raed09321@gmail.com>
2026-09-12 00:35:40 +0300

    feat(Container): add loading progress navigation indicator to enhance user experience

--- src/components/layout/Container.tsx
+++ src/components/layout/Container.tsx
@@ -1,5 +1,6 @@
+import * as React from "react";
 import { Toaster } from "react-hot-toast";
-import { Outlet } from "react-router";
+import { Outlet, useNavigation } from "react-router";
 import { useApiInterceptor } from "@/hooks";
 
 function Container() {
@@ -7,6 +8,8 @@ function Container() {
 
   return (
     <>
+      <Progress />
+
       <Outlet />
       <Toaster position="top-center" reverseOrder={false} />
     </>
@@ -14,3 +17,45 @@ function Container() {
 }
 
 export default Container;
+
+const Progress = () => {
+  const { state, location } = useNavigation();
+
+  const progressRef = React.useRef<HTMLDivElement>(null);
+
+  React.useEffect(() => {
+    if (progressRef.current) {
+      progressRef.current.style.width = "0";
+    }
+  }, [location?.pathname, progressRef]);
+
+  React.useEffect(() => {
+    if (state === "loading" && progressRef.current) {
+      let progress = 0;
+
+      const timer = setInterval(() => {
+        if (progress === 100) clearInterval(timer);
+        progress += progress > 100 ? 0 : 10;
+
+        if (progressRef.current) {
+          progressRef.current.style.width = `${progress}%`;
+        }
+      }, 300);
+
+      return () => {
+        clearInterval(timer);
+      };
+    }
+  }, [state, progressRef]);
+
+  if (state !== "loading") {
+    return null;
+  }
+
+  return (
+    <div
+      className="fixed left-0 top-0 h-1 bg-blue-500 transition-all duration-200 ease-in-out z-200"
+      ref={progressRef}
+    ></div>
+  );
+};
