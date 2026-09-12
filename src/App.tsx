cbe1212673490cae17cfeffe6095caff7f64f4dd
raed09321@gmail.com <raed09321@gmail.com>
2026-09-13 02:25:57 +0300

    refactor(App): remove unused CSS file and optimize ReactQueryDevtools import

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
+import { lazy, Suspense } from "react";
 import { QueryClientProvider } from "@tanstack/react-query";
-import "./App.css";
 import AuthProvider from "./contexts/AuthProvider";
 import ThemeProvider from "./contexts/ThemeProvider";
 import { TooltipProvider } from "./shared/components/ui/tooltip";
 import AppRouter from "./app/app.router";
 import { queryClient } from "./lib/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const ReactQueryDevtools = import.meta.env.DEV
+  ? lazy(() =>
+      import("@tanstack/react-query-devtools").then((module) => ({
+        default: module.ReactQueryDevtools,
+      })),
+    )
+  : null;
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      {import.meta.env.DEV && <ReactQueryDevtools />}
+      {ReactQueryDevtools && (
+        <Suspense fallback={null}>
+          <ReactQueryDevtools />
+        </Suspense>
+      )}
       <ThemeProvider>
         <AuthProvider>
           <TooltipProvider>
